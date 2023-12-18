1efe0cddf60b90702a870d3111a54c1842af1630
DESKTOP-M9FDRN9\csi23-iloposa <csi23-iloposa@colegioaltair.net>
2023-12-18 13:52:05 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ejercicio2DeArraysNumeros;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("---------------------");
+        int[,] matriz = new int[10, 10];
+
+        //Hacer la matriz del 100 al 1
+        int numeros = 100;
+        for (int i = 0; i < 10; i++)
+        {
+
+            for (int j = 0; j < 10; j++)
+            {
+
+                matriz[i, j] = numeros;
+                numeros--;
+
+              
+            }
+           
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(matriz[i, j] + " " );
+            }
+            Console.WriteLine("|");
+        }
+
+        Console.WriteLine("---------------------");
+        //Imprimir los numeros 
+        for (int i = 0;i <= 100; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+    
